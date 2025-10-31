cf0960d996ecb8c13c6e61c5c6d3fcf111e8b003
Livhuwani <muntswulivhucarol@gmail.com>
2025-10-31 23:18:39 +0200

    Update models.py

--- Introduction_to_Django/LibraryProject/Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Book(models.Model):
+  title = models.CharField(max_length=200)
+  author = models.CharField(mx_length=100)
+  publication_year = models.IntField()
