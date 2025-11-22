294bd5369d8ee245476584f4ae980643a0d543b5
Livhuwani <muntswulivhucarol@gmail.com>
2025-11-23 00:32:25 +0200

    Fix typo in Book model field name

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
 class Book(models.Model):
-    tittle = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
     author = models.CharField(max_length=100)
+
