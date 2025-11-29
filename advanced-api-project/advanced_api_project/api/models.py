127ba83db331085381a62230c9689940a97391d7
Livhuwani <muntswulivhucarol@gmail.com>
2025-11-29 06:15:05 +0200

    Clarify relationship in Book model with comment
    Added comment to clarify the relationship between Book and Author models.

--- advanced-api-project/advanced_api_project/api/models.py
+++ advanced-api-project/advanced_api_project/api/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 
 class Author(models.Model):
-    name = models.CharField(max_length=100)
+    name = models.CharField(max_length=100) 
 
 class Book(models.Model):
     title = models.CharField(max_length=200) 
     publication_year = models.CharField(max_length=100)
-    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE) #creates a one-many relationship
+
