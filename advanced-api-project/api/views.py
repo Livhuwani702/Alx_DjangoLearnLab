938a82eae6f5f69e9da84d09f1068a25d40b23de
Livhuwani <muntswulivhucarol@gmail.com>
2025-11-30 03:49:23 +0200

    Implement authentication for BookViewSet
    Added permission classes to BookViewSet for authentication.

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -8,3 +8,16 @@ class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
 
+from rest_framework import viewsets
+from rest_framework import permissions
+from .models import Book
+from .serializers import BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+class BookViewSet(viewsets.modelViewSet):
+    permission_classes = [IsAuthenticatedOrReadOnly]
